3c1ffffc35ecd1b80dfadbb48fb9abbc5e29a78b
Norbert250 <101406037+Norbert250@users.noreply.github.com>
2024-06-08 22:38:46 +0200

    code of africa

---
+++ Challenges/stringTransform.js
@@ -0,0 +1,22 @@
+function transformString(str) {
+    const length = str.length;
+    
+    if (length % 15 === 0) {
+        return str.split('').reverse().map(char => char.charCodeAt(0)).join(' ');
+    }
+    
+    if (length % 5 === 0) {
+        return str.split('').map(char => char.charCodeAt(0)).join(' ');
+    }
+    
+    if (length % 3 === 0) {
+        return str.split('').reverse().join('');
+    }
+    
+    return str;
+}
+
+// Example usage:
+console.log(transformString("Hamburger"));  // Output: "regrubmaH"
+console.log(transformString("Pizza"));      // Output: "80 105 122 122 97"
+console.log(transformString("Chocolate Chip Cookie"));  // Output: "eikooCpihCetalocohC"
